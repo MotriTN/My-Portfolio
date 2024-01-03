68635a67fb2b5a2a92d1ae84e07432c059d2fef0
Mohamed Trigui <68602896+MotriTN@users.noreply.github.com>
2024-01-03 01:15:10 +0100

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,26 +16,26 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/Mountains.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "Mountains Well Looking view";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm A Man Of Purpose, Discipline and Presence, I'm An IT enthusiast (especially AI)";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
+  "Polyvalent",
+  "Developer",
+  "Problems Solver",
+  "Podcast Host",
+  "Entrepeneur",
   "Graphic design",
 ];
 
